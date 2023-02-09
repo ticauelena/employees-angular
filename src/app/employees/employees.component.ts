606f5cbab502f83a4b4fb00d536f814341748111
Elena <ticauelena5@gmail.com>
2023-02-09 07:28:21 +0200

    json object part1

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent {
-
+employees = {  
+ "name" : "Rita Harford",
+ "gender" : "F",
+  "email" : "rita@gmail.com",
+  "phone" : "021312313",
+  "hireDate" : "11-10-2011",
+  "workeedType" : "In the office",
+  "salary" : 55555,
+  "isEmployee" : true
+};
 }
+
+
