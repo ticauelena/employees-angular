7bd6cc413843f840c5e721ac437d6c228168b43d
Elena <ticauelena5@gmail.com>
2023-02-17 01:07:07 +0200

    object with data

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
+import { Data } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
-
+  data = {
+    "name": "Rita Harford",
+    "gender": "F",
+    "email": "rita@gmail.com",
+    "phone": "021312313",
+    "hireDate": "11-10-2011",
+    "workeedType": "In the office",
+    "salary": 55555,
+    "isEmployee": true
+  }
 }
